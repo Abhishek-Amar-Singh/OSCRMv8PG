cce1bbfa09b348528d6b1a73ec41bdce1746dd78
Abhishek1F <abhishek.singh@1finance.org.in>
2024-06-18 17:42:58 +0530

    Shared.Lib: ToTitleCase() Changes

--- OSCRM/Shared.Lib/Extensions/ExtString.cs
+++ OSCRM/Shared.Lib/Extensions/ExtString.cs
@@ -8,7 +8,7 @@ namespace Shared.Lib.Extensions
         public static string ToLowerCase(this string input) =>
             new CultureInfo("en-US", false).TextInfo.ToLower(input);
         public static string ToTitleCase(this string input) =>
-            new CultureInfo("en-US", false).TextInfo.ToTitleCase(input);
+            char.ToUpper(input[0]) + input.Substring(1).ToLower();
         public static string ToUpperCase(this string input) =>
             new CultureInfo("en-US", false).TextInfo.ToUpper(input);
     }
